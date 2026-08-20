c7710a0cf624aa350445103d8204b3089d862629
Benjaminxy <149968944+Benjaminxy@users.noreply.github.com>
2026-08-20 13:03:50 +0000

    ajab

---
+++ Chunk/src/User.java
@@ -0,0 +1,15 @@
+package com.myapp;
+public  class Users {
+
+private String name ;
+private String username;
+private String password;
+
+
+public String getUsername () {
+
+    return username;
+}
+  
+
+}
